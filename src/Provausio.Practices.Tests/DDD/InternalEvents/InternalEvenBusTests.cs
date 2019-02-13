4a8259a16fe0d1158b833eac5debb0f7f0d04783
Jeremy Stafford <jstafford@digitalairstrike.com>
2019-02-13 15:39:46 -0700

    initial pass at internal event bus

---
+++ src/Provausio.Practices.Tests/DDD/InternalEvents/InternalEvenBusTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Provausio.Practices.DDD.InternalEvents;
+using Xunit;
+
+namespace Provausio.Practices.Tests.DDD.InternalEvents
+{
+    public class InternalEvenBusTests
+    {
+        [Fact]
+        public void ScanHandlers_FindsAllHandlers()
+        {
+            // arrange
+            var provider = new ServiceCollection().BuildServiceProvider();
+            var bus = new InternalEventBus(provider);
+            
+            // act
+            bus.ScanHandlers();
+
+            // assert
+            Assert.Equal(2, bus.Handlers.Count);
+            Assert.True(bus.Handlers.ContainsKey(typeof(FooEvent)));
+            Assert.Equal(2, bus.Handlers[typeof(FooEvent)].Count);
+        }
+
+        [Fact]
+        public async Task DispatchEvent_InvokesCorrectHandler()
+        {
+            // arrange
+            var testProvider = new Mock<IProvideNumbers>();
+            testProvider.Setup(m => m.Get());
+
+            var services = new ServiceCollection();
+            services.AddSingleton(p => testProvider.Object);
+
+            var bus = new InternalEventBus(services.BuildServiceProvider());
+
+            bus.ScanHandlers();
+            var fooEvent = new FooEvent();
+
+            // act
+            await bus.RaiseEvent(fooEvent);
+
+            /* NOTE: this actually tests that the dependency is injected, and that the handler ran */
+
+            // assert
+            testProvider.Verify(m => m.Get(), Times.Exactly(2)); // runs once per handler
+        }
+
+        private class FooHandler1 : IHandle<FooEvent>
+        {
+            private readonly IProvideNumbers numberProvider;
+
+            public FooHandler1(IProvideNumbers numberProvider)
+            {
+                this.numberProvider = numberProvider;
+            }
+
+            public Task Handle(FooEvent message)
+            {
+                var one = numberProvider.Get();
+                return Task.CompletedTask;
+            }
+        }
+
+        private class FooHandler2 : IHandle<FooEvent>
+        {
+            private readonly IProvideNumbers numberProvider;
+
+            public FooHandler2(IProvideNumbers numberProvider)
+            {
+                this.numberProvider = numberProvider;
+            }
+
+            public Task Handle(FooEvent message)
+            {
+                var one = numberProvider.Get();
+                return Task.CompletedTask;
+            }
+        }
+
+        private class BarHandler : IHandle<BarEvent>
+        {
+            public Task Handle(BarEvent message)
+            {
+                var one = 1;
+                return Task.CompletedTask;
+            }
+        }
+
+        private class FooEvent : InternalEvent
+        {
+
+        }
+
+        private class BarEvent : InternalEvent
+        {
+
+        }
+
+        public interface IProvideNumbers
+        {
+            int Get();
+        }
+
+        private class NumberProvider : IProvideNumbers
+        {
+            private readonly int _numberToProvide;
+
+            public NumberProvider(int numberToProvide)
+            {
+                _numberToProvide = numberToProvide;
+            }
+
+            public int Get()
+            {
+                return _numberToProvide;
+            }
+        }
+    }
+}
---
+++ src/Provausio.Practices/DDD/InternalEvents/IHandle.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Provausio.Practices.DDD.InternalEvents
+{
+    public class InternalEventBus
+    {
+        internal ConcurrentDictionary<Type, IList<Func<object, Task>>> Handlers 
+            = new ConcurrentDictionary<Type, IList<Func<object, Task>>>();
+
+        private readonly IServiceProvider _serviceProvider;
+
+        public InternalEventBus(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void ScanHandlers(params Assembly[] assembliesToScan)
+        {
+            var handlers = AppDomain.CurrentDomain
+                .GetAssemblies()
+                .Concat(assembliesToScan ?? new Assembly[0])
+                .SelectMany(assy => assy.GetTypes())
+                .Where(type => typeof(IInternalEventHandler).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract)
+                .ToList();
+
+            foreach (var handler in handlers)
+            {
+                var handlerInterfaces = handler
+                        .GetInterfaces()
+                        .Where(i => typeof(IInternalEventHandler).IsAssignableFrom(i) 
+                                    && i != typeof(IInternalEventHandler));
+
+                foreach (var handlerDeclaration in handlerInterfaces)
+                {
+                    var messageType = handlerDeclaration.GetGenericArguments().Single();
+                    UpdateHandlers(messageType, (msg) =>
+                    {
+                        var handlerInstance = ActivatorUtilities.CreateInstance(_serviceProvider, handler);
+                        return (Task) handlerInstance
+                            .GetType().GetMethod("Handle")
+                            .Invoke(handlerInstance, new []{msg});
+                    });
+                }
+            }
+        }
+
+        private void UpdateHandlers(Type messageType, Func<object, Task> handler)
+        {
+            if(!Handlers.ContainsKey(messageType))
+                Handlers.TryAdd(messageType, new List<Func<object, Task>>());
+
+            if(!Handlers[messageType].Contains(handler))
+                Handlers[messageType].Add(handler);
+        }
+
+        public async Task RaiseEvent<T>(T message) 
+            where T : InternalEvent
+        {
+            // TODO: optimize and throttle etc.
+
+            if(!Handlers.ContainsKey(typeof(T)))
+                return;
+
+            var handlers = Handlers[typeof(T)];
+            var dispatched = new List<Task>();
+            foreach(var handler in handlers)
+                dispatched.Add(handler(message));
+
+            await Task.WhenAll(dispatched);
+        }
+    }
+
+    public class InternalEvent
+    {
+
+    }
+
+    public interface IInternalEventHandler
+    {
+
+    }
+
+    /// <summary>
+    /// Designates the class as a handler of an <see cref="InternalEvent"/>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IHandle<T> : IInternalEventHandler
+        where T : InternalEvent
+    {
+        Task Handle(T message);
+    }
+}
